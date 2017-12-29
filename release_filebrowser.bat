020ec60bc45c58c4b940b278bcadb50621be1cd4
giofrida <giofrida@users.noreply.github.com>
2017-12-29 23:09:46 +0100

    Added batches to copy the required files
    Just execute one of them and the files needed will be copied in the folder "release", ready to be transferred over ftp

---
+++ release_filebrowser.bat
@@ -0,0 +1,4 @@
+xcopy UI\hisenseUI\css\fileBrowser.css                  release\css\fileBrowser.css
+xcopy UI\hisenseUI\modulePages\himedia\fileBrowser.js   release\modulePages\himedia\fileBrowser.js
+xcopy UI\hisenseUI\tempString\hiMediaString.js          release\tempString\hiMediaString.js
+xcopy UI\hisenseUI\mediaIndex.html                      release\mediaIndex.html
---
+++ release_videoplayer.bat
@@ -0,0 +1,2 @@
+xcopy UI\hisenseUI\css\videoPlayer.css                 release\css\videoPlayer.css*
+xcopy UI\hisenseUI\modulePages\himedia\videoPlayer.js  release\modulePages\himedia\videoPlayer.js*
